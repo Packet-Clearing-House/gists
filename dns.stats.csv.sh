648cd09ac144e07ad36825e69d702fb4dca4c959
Ths2-9Y-LqJt6 <mrjones@pch.net>
2018-11-01 22:00:00 -0700

    update DNS stats bash script to point to new www host

--- dns.stats.csv.sh
+++ dns.stats.csv.sh
@@ -2,15 +2,25 @@
 
 #
 # Packet Clearing House
-# c2016
+# c2018
 # MIT License
 # https://github.com/Packet-Clearing-House/gists
 #
 
-# To use this script you'll need to swap in your own USERNAME, PASSWORD, TLD
-# and DATE below. DATE should be YEARMONTHDAY format like "20160401". TLD should have the
-# leading dot like ".com".  Finally, be sure to keep the ampersands (&) escaped
-# in the final URL
+# To use this script you'll need to swap in your own USERNAME, PASSWORD, TLD,
+# ACTION and DATE below. DATE should be YEARMONTHDAY format like "20160401".
+#
+#
+# The two actions are "dns_stats_table_city" and "dns_stats_table_time":
+#
+#   By City: https://www.pch.net/api/anycast/dns_stats_table_city/TLD/DATE
+#   By Time: https://www.pch.net/api/anycast/dns_stats_table_time/TLD/DATE
+#
+# You can log into this URL to see all your TLDs available, as well as view
+# more help on which URLs to use:
+#
+#   https://www.pch.net/account/anycast/stats
+#
 
 echo ".pch.net	TRUE	/	FALSE	1776643854	pch_nonce100	100" > cookies.txt
 
@@ -22,11 +32,9 @@ wget --delete-after \
      https://www.pch.net/user/login
 
 wget --continue \
-	--wait=10 \
-	--random-wait \
 	--execute robots=off \
 	--load-cookies cookies.txt \
 	--no-parent \
-	--output-document pch_query_counts.csv \
-    https://prefix.pch.net/applications/anycast-dns/query_counts.php?mode=time\&tld=TLD\&date=DATE
+	--output-document dns_stats.csv \
+    https://www.pch.net/api/anycast/ACTION/TLD/DATE
 # NB trailing slash
